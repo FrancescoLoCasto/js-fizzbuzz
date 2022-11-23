9dfd659b4a8af4a666a1aeb1bd3b434b6c8b621a
MyUsername <my@email.com>
2022-11-23 15:13:02 +0100

    insert let content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
+      //Consegna
+
 // Scrivi un programma che stampi in console i numeri da 1 a 100: ma che per i multipli di 3 stampi “Fizz” al posto del numero eper i multipli di 5 stampi “Buzz”.Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.  
 
 // let section
 
+let Fizz = document.getElementById("Fizz")
+let Buzz = document.getElementById("Buzz")
+let FizzBuzz = document.getElementById("FizzBuzz")
+let fizzNumbers = document.getElementById("fizzNumbers")
